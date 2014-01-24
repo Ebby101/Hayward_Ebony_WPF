50f5999973749aef3dd88bff852f0ef4469ecd62
Ebony Hayward <ebonysh@gmail.com>
2014-01-23 21:53:05 -0600

    initial commit

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -6,36 +6,47 @@
 
 //Does the competitor's weight qualify for the division/
 
-var weight = 250;
+//var weight = 250;
 //if the weight is more than 250/
 
 
-if (weight <= 250) {
+//if (weight <= 250) {
     //if it’s less than 250/
-    console.log(“var competitor;
-    competitor
-    var needs;
-    needs
-    to
-    gain
-    more
-    weight
-    !”)
-    ;
+    //console.log(“var competitor;//
+    //competitor
+    //var needs;
+    //needs
+    //to
+    //gain
+   // more
+    //weight
+    //!”)
+    //;
 
-} else {
+//} else {
     //if its more than 250/
-    console.log(“The
-    competitor
-    qualifies”)
-    ;
-}
+    //console.log(“The
+   // competitor
+    //qualifies”)//
+   // ;
+//}//
 
-//Expressions Conditionals
+//Expressions Conditionals - Last Chance for Gas
 //A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
 
 //Given:Gas efficiency of the car (in mpg)
 //Gauge reading of the gas tank (in %)
 //Car’s gas tank capacity (in gallons)
 //Result To Print Out:
-//  “Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”//
+//“Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”//
+
+var carMilage = 40
+var gauge = 10%
+var gallons = 80
+
+if(carMilage > gauge) && (gallons < gauge);
+    //if both are true then yes gas is needed
+    console.log("Yes, you can make it without stopping for gas!")
+if(carMilage < gauge) && (gallons > gauge);
+    console.log("You only have X gallons of gas in your tank, better get gas now while you can!")
+
