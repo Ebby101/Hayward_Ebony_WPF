bf16d7139aa737b35fdb2d66ee475853fa05447b
Ebony Hayward <ebonysh@gmail.com>
2014-01-23 21:37:01 -0600

    initial commit

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -10,13 +10,32 @@ var weight = 250;
 //if the weight is more than 250/
 
 
-if(weight > 250){
-    //if its more than 250/
-    console.log(“The competitor qualifies”);
-}else{
+if (weight <= 250) {
     //if it’s less than 250/
-    console.log(“The competitor needs to gain more weight!”);
+    console.log(“var competitor;
+    competitor
+    var needs;
+    needs
+    to
+    gain
+    more
+    weight
+    !”)
+    ;
 
+} else {
+    //if its more than 250/
+    console.log(“The
+    competitor
+    qualifies”)
+    ;
 }
 
-//
+//Expressions Conditionals
+//A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+
+//Given:Gas efficiency of the car (in mpg)
+//Gauge reading of the gas tank (in %)
+//Car’s gas tank capacity (in gallons)
+//Result To Print Out:
+//  “Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”//
