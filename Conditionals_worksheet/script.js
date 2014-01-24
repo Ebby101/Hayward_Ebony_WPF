a9c6f519c42c2b64dc0d99f4eee5406423d805ba
Ebony Hayward <ebonysh@gmail.com>
2014-01-23 21:30:23 -0600

    initial commit

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -1,3 +1,22 @@
 /**
- * Created by ebonyhayward on 1/23/14.
+ * Created by ebonyhayward on 1/23/14. Conditionals_Worksheet
  */
+
+/**stuff your face I*/
+
+//Does the competitor's weight qualify for the division/
+
+var weight = 250;
+//if the weight is more than 250/
+
+
+if(weight > 250){
+    //if its more than 250/
+    console.log(“The competitor qualifies”);
+}else{
+    //if it’s less than 250/
+    console.log(“The competitor needs to gain more weight!”);
+
+}
+
+//
